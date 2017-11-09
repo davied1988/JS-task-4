d5ed007b6337cfb9af9c2cd43fc27e4db6208f47
Dawid G <dawid.grudziecki@gmail.com>
2017-11-09 02:03:38 +0100

    task number 4 files

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+var a = 4,
+	h = 3,
+	triangleArea = a*h/2;
+	
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+var base = prompt('Enter you\'re base value');
+var height = prompt('Enter you\'re height value');
+console.log(base*height/2);
+
+	
